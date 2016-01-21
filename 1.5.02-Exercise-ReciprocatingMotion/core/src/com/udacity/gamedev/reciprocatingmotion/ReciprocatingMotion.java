707d0cb3da3bb3e4bcefd8f92024eee8f0d88c5f
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2016-01-21 21:28:21 +0100

    reciprocating motion exercise

--- 1.5.02-Exercise-ReciprocatingMotion/core/src/com/udacity/gamedev/reciprocatingmotion/ReciprocatingMotion.java
+++ 1.5.02-Exercise-ReciprocatingMotion/core/src/com/udacity/gamedev/reciprocatingmotion/ReciprocatingMotion.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /**
@@ -23,13 +25,13 @@ public class ReciprocatingMotion extends ApplicationAdapter {
     private static final float MOVEMENT_DISTANCE = WORLD_SIZE / 4;
 
     // TODO: Define a constant that fixes how long a cycle of the animation should take in seconds
-
+    private static final int CYCLE = 2;
 
     ShapeRenderer renderer;
     ExtendViewport viewport;
 
     // TODO: Create a long to hold onto ApplicationAdapter creation time
-
+    long appCreationTime;
 
     @Override
     public void create() {
@@ -37,7 +39,7 @@ public class ReciprocatingMotion extends ApplicationAdapter {
         viewport = new ExtendViewport(WORLD_SIZE, WORLD_SIZE);
 
         // TODO: Save current value of TimeUtils.nanoTime()
-
+        appCreationTime = TimeUtils.nanoTime();
     }
 
     @Override
@@ -65,21 +67,20 @@ public class ReciprocatingMotion extends ApplicationAdapter {
         float worldCenterY = viewport.getWorldHeight() / 2;
 
         // TODO: Figure out how long it's been since the animation started using TimeUtils.nanoTime()
-
+        long delta = TimeUtils.nanoTime() - appCreationTime;
 
         // TODO: Use MathUtils.nanoToSec to figure out how many seconds the animation has been running
-
+        float seconds = delta * MathUtils.nanoToSec;
 
         // TODO: Figure out how many cycles have elapsed since the animation started running
-
+        float cycles = seconds / CYCLE;
 
         // TODO: Figure out where in the cycle we are
-
+        float cyclePosition = cycles % 1;
 
         // TODO: Use MathUtils.sin() to set the x position of the circle
 
-
-        float x = worldCenterX;
+        float x = worldCenterX + MOVEMENT_DISTANCE * MathUtils.sin(MathUtils.PI2 * cyclePosition);
         float y = worldCenterY;
         renderer.circle(x, y, CIRCLE_RADIUS);
         renderer.end();
